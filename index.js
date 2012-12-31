2aa527d94c1fafe54727ee36e85c2a5089ee8144
Steve Streza <stevestreza@gmail.com>
2012-12-31 11:21:32 -0800

    Emit events and handle no-callback cases better

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-exports.connection = require("./lib/connection");
-exports.job = require("./lib/job");
-exports.worker = require("./lib/worker");
+var _pq = function(){};
+require("util").inherits(_pq, require("events").EventEmitter);
+
+var pq = new _pq();
+
+module.exports = pq;
+
+pq.connection = require("./lib/connection");
+pq.job = require("./lib/job");
+pq.worker = require("./lib/worker");
+
+pq.connection.pq = pq;
